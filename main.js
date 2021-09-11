bc38f1c57325bd2757db2d10107aee75778f649d
Chris Durden <cdurden@gmail.com>
2021-09-11 13:35:07 -0500

    adding selector

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 console.log("Hei");
 console.log(document);
+var wordElements = document.querySelectorAll("span");
+console.log(wordElements.length);
+wordElements.forEach(function (element) {
+    console.log(element.innerHTML);
+});
