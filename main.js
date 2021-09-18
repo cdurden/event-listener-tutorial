7f325a5900882a3dbd598f5663e6a90b71705b46
Chris Durden <cdurden@gmail.com>
2021-09-17 23:25:21 -0500

    Lookup word from Aleksi server on click event

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ var wordElements = document.querySelectorAll("span");
 console.log(wordElements.length);
 wordElements.forEach(function (element) {
     element.addEventListener("click", function () {
-        console.log(element.innerHTML);
+        axios
+            .get(
+                `https://dev.aleksi.org/words/${element.innerHTML}?language=fi&languageTo=en`
+            )
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            });
     });
 });
