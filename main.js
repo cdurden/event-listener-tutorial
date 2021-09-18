8ff06f9a020a79432e9a473154f03a1284c85360
Chris Durden <cdurden@gmail.com>
2021-09-17 23:21:35 -0500

    Print word under cursor on click event

--- main.js
+++ main.js
@@ -3,5 +3,7 @@ console.log(document);
 var wordElements = document.querySelectorAll("span");
 console.log(wordElements.length);
 wordElements.forEach(function (element) {
-    console.log(element.innerHTML);
+    element.addEventListener("click", function () {
+        console.log(element.innerHTML);
+    });
 });
