a1ca63ae767d59fcf7aef688ffd8c28798ed38e4
Chris Durden <cdurden@gmail.com>
2021-09-17 23:43:13 -0500

    Outputs one row from translations data

--- main.js
+++ main.js
@@ -2,6 +2,23 @@ console.log("Hei");
 console.log(document);
 var wordElements = document.querySelectorAll("span");
 console.log(wordElements.length);
+function createOutputTable(data) {
+    const table = document.createElement("table");
+    data.lemmas.forEach(function (lemma) {
+        const tableRow = document.createElement("tr");
+        const tableLemmaColumnItem = document.createElement("td");
+        tableLemmaColumnItem.innerHTML = lemma.lemma;
+        const tableTranslationColumnItem = document.createElement("td");
+        tableTranslationColumnItem.innerHTML =
+            lemma.translations.length > 0
+                ? lemma.translations[0].translation
+                : "";
+        tableRow.appendChild(tableLemmaColumnItem);
+        tableRow.appendChild(tableTranslationColumnItem);
+        table.appendChild(tableRow);
+    });
+    return table;
+}
 wordElements.forEach(function (element) {
     element.addEventListener("click", function () {
         axios
@@ -9,6 +26,8 @@ wordElements.forEach(function (element) {
                 `https://dev.aleksi.org/words/${element.innerHTML}?language=fi&languageTo=en`
             )
             .then(function (response) {
+                const table = createOutputTable(response.data);
+                document.querySelector("#output table").replaceWith(table);
                 console.log(response);
             })
             .catch(function (error) {
