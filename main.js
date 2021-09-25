308f59da7db1ec7a4ee9cd60fcd4a76666652a77
Chris Durden <cdurden@gmail.com>
2021-09-25 16:48:45 -0500

    This demonstrates that we can access the enclosing function's scope from multiple separate event listeners

--- main.js
+++ main.js
@@ -2,8 +2,25 @@ console.log("Hei");
 console.log(document);
 var wordElements = document.querySelectorAll("span");
 console.log(wordElements.length);
-wordElements.forEach(function (element) {
-    element.addEventListener("click", function () {
-        console.log(element.innerHTML);
+function addEventListeners(modCode) {
+    var mod = false;
+    function noop(e) {}
+    document.addEventListener("click", function (e) {
+        if (mod) {
+            console.log("Mod active");
+        } else {
+            console.log("Mod inactive");
+        }
     });
-});
+    document.addEventListener("keydown", function (e) {
+        if (e.code === modCode) {
+            mod = true;
+        }
+    });
+    document.addEventListener("keyup", function (e) {
+        if (e.code === modCode) {
+            mod = false;
+        }
+    });
+}
+addEventListeners("ControlLeft");
