ecc8831d4b76a893327af406919026ce06f5f06a
Chris Durden <cdurden@gmail.com>
2021-11-13 08:08:15 -0600

    Added checkbox control of link behavior

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
 console.log("Hei");
-console.log(document);
+
 var wordElements = document.querySelectorAll("span");
-console.log(wordElements.length);
-wordElements.forEach(function (element) {
-    console.log(element.innerHTML);
+
+document.addEventListener("click", function (e) {
+    if (document.getElementById("alt-click").checked) {
+        console.log("Default link behavior disabled");
+        if (e.target.id !== "alt-click") {
+            e.preventDefault();
+        }
+    } else {
+        console.log("Links enabled");
+    }
 });
+
+/*
+const mod = false;
+const modCode = "ControlLeft";
+document.addEventListener("keydown", function (e) {
+    if (e.code === modCode) {
+        mod = true;
+    }
+});
+document.addEventListener("keyup", function (e) {
+    if (e.code === modCode) {
+        mod = false;
+    }
+});
+*/
