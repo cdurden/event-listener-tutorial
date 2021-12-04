b196b51063773fc1ab7df27d567e113120ed29a5
Chris Durden <cdurden@gmail.com>
2021-12-04 10:53:04 -0600

    Timers demo of closures

---
+++ timers.js
@@ -0,0 +1,36 @@
+var n = 0;
+function createTimerElmt(n) {
+    const timersContainer = document.getElementById("timers");
+    const timerContainer = document.createElement("div");
+    timersContainer.appendChild(timerContainer);
+    timerContainer.style =
+        "border: 1px solid black; float: left; margin: 10px; padding: 10px;";
+    const heading = document.createElement("h1");
+    heading.innerHTML = `Timer ${n}`;
+    timerContainer.appendChild(heading);
+    const timeRemainingElmt = document.createElement("span");
+    timerContainer.appendChild(timeRemainingElmt);
+    const timerMessage = document.createElement("p");
+    timerContainer.appendChild(timerMessage);
+    return [timeRemainingElmt, timerMessage];
+}
+function addTimer(secondsRemaining) {
+    n += 1;
+    const [timeRemainingElmt, timerMessage] = createTimerElmt(n);
+    timeRemainingElmt.innerHTML = secondsRemaining;
+    const timerInterval = setInterval(function () {
+        secondsRemaining -= 1;
+        if (secondsRemaining <= 0) {
+            clearInterval(timerInterval);
+            timerMessage.innerHTML = `Timer expired at ${new Date()}`;
+        }
+        timeRemainingElmt.innerHTML = secondsRemaining;
+    }, 1000);
+}
+document.addEventListener("DOMContentLoaded", function () {
+    document
+        .getElementById("addTimerButton")
+        .addEventListener("click", function () {
+            addTimer(document.getElementById("timerMinutesInput").value * 60);
+        });
+});
